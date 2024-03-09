7d40bc629c465ee430add0b0820c1ceee7e5151d
Feyza Ateşoğlu <atesogluf35@gmail.com>
2024-03-09 09:30:21 +0300

    Firebase connection provided.

--- ng-app/src/app/app.module.ts
+++ ng-app/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import  {AngularFireModule} from '@angular/fire/compat';
+import {AngularFireAuthModule} from '@angular/fire/compat/auth';
 
 @NgModule({
   declarations: [		
@@ -14,7 +16,17 @@ import { RegisterComponent } from './register/register.component';
    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyBNwwQ6tgYlrC5qCH_fklwjouA3ORh2Ios",
+      authDomain: "firststep-auth.firebaseapp.com",
+      projectId: "firststep-auth",
+      storageBucket: "firststep-auth.appspot.com",
+      messagingSenderId: "25710646588",
+      appId: "1:25710646588:web:ba8efb68df754449997a39"
+    }),
+    AngularFireAuthModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
