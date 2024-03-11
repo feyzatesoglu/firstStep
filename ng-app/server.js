469dffd9e5f24d37164f5dbe803f93e0716c5049
Feyza Ateşoğlu <atesogluf35@gmail.com>
2024-03-11 16:34:05 +0300

    hosting processes

--- ng-app/server.js
+++ ng-app/server.js
@@ -11,7 +11,7 @@ app.get('*', (req, res) => {
 });
 
 // Start the server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4200;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
