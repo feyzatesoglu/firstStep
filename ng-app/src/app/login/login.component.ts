4290e9b0bb91878008e7338dcb0e8002a4bf5401
Feyza Ateşoğlu <atesogluf35@gmail.com>
2024-03-31 16:29:47 +0300

    feat: use reactive form in Login Component

--- ng-app/src/app/login/login.component.ts
+++ ng-app/src/app/login/login.component.ts
@@ -2,41 +2,48 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthenticationService } from '../services/authentication.service';
 import { Router } from '@angular/router';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
   selector: 'app-login',
   templateUrl: './login.component.html',
 })
 export class LoginComponent implements OnInit {
 
-  form!: FormGroup;
-  isLoggingIn=false;
+  isLoggingIn = false;
 
   constructor(
     private formBuilder: FormBuilder,
-    private authenticationService:AuthenticationService,
-    private router:Router
-  ) { }
-  
-  email!: string;
-  password!: string;
+    private authenticationService: AuthenticationService,
+    private router: Router
+  ) {}
+
+form!:FormGroup;
+
 
   ngOnInit(): void {
-    this.form=this.formBuilder.group({
-      email:['', [Validators.required, Validators.email]],
-      password:['',[Validators.required]]
-    })
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
   }
-  login(){
-this.isLoggingIn=true;
-this.authenticationService.signWithEmailandPassword(this.email,this.password).then(res=>{
-  console.log('Login successful');
-  this.router.navigate(['/profile']);
-}).catch(err=>console.error('login failed',err));
-}
-loginWithGoogle(){
-  this.authenticationService.SignInWithGoogle();
-  this.router.navigate(['profile']);
-}
+  login() {
 
+  const{email,password}=this.form.value;
+
+ this.authenticationService
+      .signWithEmailandPassword(email, password)
+      .then((res) => {
+        console.log('Login successful');
+        this.router.navigate(['/profile']);
+      })
+      .catch((err) => alert(err));
+ }
+  loginWithGoogle() {
+    this.authenticationService.SignInWithGoogle();
+    this.router.navigate(['profile']);
+  }
 }
