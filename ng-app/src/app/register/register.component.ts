6617aa42ba1541047aa4e0d53aa2d4071365a177
Feyza Ateşoğlu <atesogluf35@gmail.com>
2024-03-31 17:16:07 +0300

    feat: use reactive form in Register Component

--- ng-app/src/app/register/register.component.ts
+++ ng-app/src/app/register/register.component.ts
@@ -1,32 +1,51 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthenticationService } from '../services/authentication.service';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
+  standalone:true,
+  imports: [ReactiveFormsModule, CommonModule],
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
 
+  registerForm!:FormGroup;
   constructor(
-    //private formBuilder: FormBuilder,
+    private formBuilder: FormBuilder,
     private authenticationService:AuthenticationService,
     private router:Router
   ) { }
-  username!:string;
-  email!: string;
-  password!: string;
+
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]
+    });
+    validators: this.passwordMatchValidator
   }
   register() {
-    this.authenticationService.registerWithEmailandPassword(this.email, this.password)
+    const{email,password}=this.registerForm.value;
+    this.authenticationService.registerWithEmailandPassword(email,password)
       .then(res => {
         console.log('Registration successful');
         this.router.navigate(['/login']);
       })
       .catch(err => console.error('Registration failed:', err));
   }
+  passwordMatchValidator(formGroup: FormGroup) {
+    const passwordControl = formGroup.get('password');
+    const confirmPasswordControl = formGroup.get('confirmPassword');
+  
+    if (passwordControl?.value === confirmPasswordControl?.value) {
+      confirmPasswordControl?.setErrors(null);
+    } else {
+      confirmPasswordControl?.setErrors({ passwordMismatch: true });
+    }
   }
- 
+  
+}
