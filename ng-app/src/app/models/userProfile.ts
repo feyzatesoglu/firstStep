5831404fcffb931313b57bc08bea8169f690858c
Feyza Ateşoğlu <atesogluf35@gmail.com>
2024-04-07 23:55:44 +0300

    feat: create models folder and files

---
+++ ng-app/src/app/models/userProfile.ts
@@ -0,0 +1,9 @@
+export interface UserProfile {
+    uid: string;
+    email: string;
+    displayName?:string;
+    photoURL?:string;
+    firstName?: string;
+    phone?: string;
+    address?: string;
+}
